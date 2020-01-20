c2bb4fba424aaf5b06f2f04366589fa8ce622364
anantsaxena <anantsaxena1993@gmail.com>
2020-01-20 22:11:18 +0530

    commit for IOC

---
+++ SpringDemoIOC/src/com/springDemo/FirstSpringApp.java
@@ -0,0 +1,14 @@
+package com.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class FirstSpringApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach coach = context.getBean("myCoach", Coach.class);
+		System.out.println(coach.getDailyWorkout());
+		context.close();
+	}
+}
+ 
---
+++ SpringDemoIOC/src/com/springDemo/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.springDemo;
+
+public class TrackCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout(){
+		return "Run for 5km daily";
+	}
+}
